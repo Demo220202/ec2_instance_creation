f106afd94f26e22d75d7bf7d0bb52b6d89479e45
Demo220202 <158024374+Demo220202@users.noreply.github.com>
2024-07-28 10:55:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_pair
+
+  tags = {
+    Name = var.instance_name
+    "${var.additional_tag_name}" = var.additional_tag_value
+  }
+
+  root_block_device {
+    volume_size = var.storage
+  }
+
+  vpc_security_group_ids = [var.security_group]
+  iam_instance_profile   = var.iam_role
+}
