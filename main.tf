22b2d92189667cbe0cd0248645448b7bbd6892c0
Demo220202 <158024374+Demo220202@users.noreply.github.com>
2024-07-28 11:18:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,19 @@ provider "aws" {
   region = var.region
 }
 
+variable "region" {}
+variable "instance_type" {}
+variable "storage" {}
+variable "security_group" {}
+variable "iam_role" {}
+variable "iam_instance_profile" {}
+variable "key_pair" {}
+variable "vpc_id" {}
+variable "ami_id" {}
+variable "instance_name" {}
+variable "additional_tag_name" {}
+variable "additional_tag_value" {}
+
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
@@ -17,5 +30,10 @@ resource "aws_instance" "example" {
   }
 
   vpc_security_group_ids = [var.security_group]
-  iam_instance_profile   = var.iam_role
+  iam_instance_profile   = aws_iam_instance_profile.example.name
+}
+
+resource "aws_iam_instance_profile" "example" {
+  name = var.iam_instance_profile
+  role = var.iam_role
 }
