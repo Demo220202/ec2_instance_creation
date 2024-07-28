0e2be003ea99a99f76fa41893da4aaae1c6f4922
Demo220202 <158024374+Demo220202@users.noreply.github.com>
2024-07-28 10:52:48 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+}
+
+variable "storage" {
+  description = "Storage Size in GB"
+  type        = number
+}
+
+variable "security_group" {
+  description = "Existing Security Group ID"
+  type        = string
+}
+
+variable "iam_role" {
+  description = "IAM Role to be assigned"
+  type        = string
+}
+
+variable "key_pair" {
+  description = "Key Pair Name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name of the Instance"
+  type        = string
+}
+
+variable "additional_tag_name" {
+  description = "Additional Tag Name"
+  type        = string
+}
+
+variable "additional_tag_value" {
+  description = "Additional Tag Value"
+  type        = string
+}
